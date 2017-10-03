0bdb744d57473fee1d35521370ae068143669f3b
Deanna Lepke <deannalepke@gmail.com>
2017-10-03 18:01:22 +0000

    completed basic user interface mockup for chattyapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
+class NavBar extends Component {
+  render() {
+    console.log("Rendering <NavBar />");
+    return (
+      <div className = "navbar">
+        <nav>
+        <span className="header"><h1 className="navbar-brand">Chatty</h1></span>
+        </nav>
+      </div>
+    );
+  }
+}
+
+
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
